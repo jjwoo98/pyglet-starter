39144d0f45dc90dd1e04e5fcd9d8b7fb82212782
me <60404073+jjwoo98@users.noreply.github.com>
2020-02-27 13:24:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,119 @@
-import pyglet
-
-win= pyglet.window.Window()
-
-# Create a sprite so the game can keep track of coordinates
-img= pyglet.image.load('assets/gfx/character.png')
-smol_img = img.get_region(x = 72, y = 130, width = 24, height = 25)
-spr = pyglet.sprite.Sprite(smol_img, x = 200, y = 200)
-
-
-# Get the key state handler object
-keys = pyglet.window.key.KeyStateHandler()
-
-def update(dt):
-    win.push_handlers(keys)             # update the keys object
-    if keys[pyglet.window.key.LEFT]: # if the key is pressed...
-        spr.x -= 5                             # change spr.x by -1
-    if keys[pyglet.window.key.RIGHT]:
-        spr.x += 5
-    if keys[pyglet.window.key.UP]:
-        spr.y += 5
-    if keys[pyglet.window.key.DOWN]:
-        spr.y -= 5
-
+"""
+ Simple snake example.
  
-@win.event
-def on_draw():
-    win.clear()
-    spr.draw()
-
-pyglet.clock.schedule(update) 
-pyglet.app.run()
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+"""
+ 
+import pyglet
+ 
+# --- Globals ---
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+# Set the width and height of each snake segment
+segment_width = 15
+segment_height = 15
+# Margin between each segment
+segment_margin = 3
+ 
+# Set initial speed
+x_change = segment_width + segment_margin
+y_change = 0
+ 
+ 
+class Segment(pyglet.sprite.Sprite):
+    """ Class to represent one segment of the snake. """
+    # -- Methods
+    # Constructor function
+    def __init__(self, x, y):
+        # Call the parent's constructor
+        super().__init__()
+ 
+        # Set height, width
+        self.image = pyglet.Surface([segment_width, segment_height])
+        self.image.fill(WHITE)
+ 
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+ 
+# Call this function so the Pygame library can initialize itself
+pyglet.init()
+ 
+# Create an 800x600 sized screen
+screen = pyglet.display.set_mode([800, 600])
+ 
+# Set the title of the window
+pyglet.display.set_caption('Snake Example')
+ 
+allspriteslist = pyglet.sprite.Group()
+ 
+# Create an initial snake
+snake_segments = []
+for i in range(15):
+    x = 250 - (segment_width + segment_margin) * i
+    y = 30
+    segment = Segment(x, y)
+    snake_segments.append(segment)
+    allspriteslist.add(segment)
+ 
+ 
+clock = pyglet.time.Clock()
+done = False
+ 
+while not done:
+ 
+    for event in pyglet.event.get():
+        if event.type == pyglet.QUIT:
+            done = True
+ 
+        # Set the speed based on the key pressed
+        # We want the speed to be enough that we move a full
+        # segment, plus the margin.
+        if event.type == pyglet.KEYDOWN:
+            if event.key == pyglet.K_LEFT:
+                x_change = (segment_width + segment_margin) * -1
+                y_change = 0
+            if event.key == pyglet.K_RIGHT:
+                x_change = (segment_width + segment_margin)
+                y_change = 0
+            if event.key == pyglet.K_UP:
+                x_change = 0
+                y_change = (segment_height + segment_margin) * -1
+            if event.key == pyglet.K_DOWN:
+                x_change = 0
+                y_change = (segment_height + segment_margin)
+ 
+    # Get rid of last segment of the snake
+    # .pop() command removes last item in list
+    old_segment = snake_segments.pop()
+    allspriteslist.remove(old_segment)
+ 
+    # Figure out where new segment will be
+    x = snake_segments[0].rect.x + x_change
+    y = snake_segments[0].rect.y + y_change
+    segment = Segment(x, y)
+ 
+    # Insert new segment into the list
+    snake_segments.insert(0, segment)
+    allspriteslist.add(segment)
+ 
+    # -- Draw everything
+    # Clear screen
+    screen.fill(BLACK)
+ 
+    allspriteslist.draw(screen)
+ 
+    # Flip screen
+    pyglet.display.flip()
+ 
+    # Pause
+    clock.tick(5)
+ 
+pyglet.quit()
